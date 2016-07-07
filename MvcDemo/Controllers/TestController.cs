da6781f4fcb4545bf4bfab2a208a6d3c6d06b904
LanternLee <lanternismn@126.com>
2016-07-07 15:54:07 +0800

    FirstShoot

---
+++ MvcDemo/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcDemo.DAL;
+
+namespace MvcDemo.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        private Entities Db = new Entities();
+        public ActionResult Index()
+        {
+            List<GENRE> genres = Db.GENRE.ToList<GENRE>();
+            return View(genres);
+        }
+    }
+}
